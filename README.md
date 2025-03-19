9bfaae22ac5f03015733c4d8ec1321516f8c5dc3
shu-kitamura <shusei3316@yahoo.co.jp>
2025-03-20 00:14:00 +0900

    add source code and update README.md

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # crtime
+
+A simple Rust CLI tool that print the creation datetime of a given file in local time.
+
+## Installation
+
+```bash
+# Clone this repository
+git clone https://github.com/shu-kitamura/crtime.git
+cd crtime
+
+# Build the binary
+cargo build --release
+
+# (Optional) Install globally
+cargo install --path .
+```
+
+## Usage
+
+```
+Usage: crtime <FILEPATH>
+
+Arguments:
+  <FILEPATH>  The file path of the file to check the creation datetime
+
+Options:
+  -h, --help  Print help
+```
+
+## Example
+
+```
+$ crtime ~/path/to/src/main.rs 
+2025/03/19 21:25:47
+```
+
---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use std::fs::metadata;
+use chrono::{prelude::DateTime, Local};
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    /// The file path of the file to check the creation datetime.
+    #[arg(value_name = "FILEPATH")]
+    filename: String,
+}
+
+fn main() {
+    let args = Args::parse();
+    let created_timestamp = match metadata(args.filename) {
+        Ok(m) => match m.created() {
+            Ok(st) => st,
+            Err(e) => return eprintln!("{e}")
+        },
+        Err(e) => return eprintln!("{e}")
+    };
+
+    let local_time: DateTime<Local> = created_timestamp.into();
+    println!("{}", local_time.format("%Y/%m/%d %T"));
+}
